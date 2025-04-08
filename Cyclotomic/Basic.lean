cfc24a9c24f82629309a206e4d540e3b6d7a9b90
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-04-08 12:08:52 +0200

    start

--- Cyclotomic/Basic.lean
+++ Cyclotomic/Basic.lean
@@ -1,1 +1,10 @@
-def hello := "world"
+import Mathlib
+
+variable {K : Type*} [Field K] [Fintype K] {p f n : ℕ} [Fact p.Prime]
+  (hK : Fintype.card K = p^f) (hn : (p^f).Coprime n)
+
+open Polynomial ZMod
+
+theorem foo {P : K[X]} (hP : P ∣ cyclotomic n K) (hPirr : Irreducible P) :
+    orderOf (unitOfCoprime _ hn) ≤ P.natDegree := by
+  sorry
